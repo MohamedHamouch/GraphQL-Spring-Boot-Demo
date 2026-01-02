d4719a7172b978ea431e4402637a314f4422d565
Mohamed Hamouch <med10hamouch@gmail.com>
2026-01-02 06:37:51 +0100

    Update dependencies and app proprties

--- src/main/java/com/example/graphqldemo/GraphQlDemoApplication.java
+++ src/main/java/com/example/graphqldemo/GraphQlDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GraphQlDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphQlDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlDemoApplication.class, args);
+    }
 
 }
